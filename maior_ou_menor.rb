9533edcee57002c3d0e5ba065cf7001d2bbf53e4
Your Name <you@example.com>
2017-03-21 03:32:56 -0300

    Code smell- [REMOVIDO] copy e paste -test- Versao laco for mais simples

--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -6,8 +6,9 @@ puts "Começaremos o jogo para você, " + nome
 puts "Escolhendo um número secreto entre 0 e 200..."
 numero_secreto = 175
 puts "Escolhido... que tal adivinhar hoje nosso número secreto?"
+for tentativa in 1..6
 puts "\n\n\n\n"
-puts "Tentativa 1"
+puts "Tentativas " + tentativa.to_s + "de 3"
 puts "Entre com o numero"
 chute = gets
 puts "Será que acertou? Você chutou " + chute
@@ -21,37 +22,6 @@ else
         puts "Errou, o número secreto é maior!"
     else
         puts "Errou, o número secreto é menor!"
-    end
-end
-puts "Tentativa 2"
-puts "Entre com o numero"
-chute = gets
-puts "Será que acertou? Você chutou " + chute
-puts "\n\n\n\n"
-acertou = numero_secreto  == chute.to_i
-if acertou
-    puts "Acertou!"
-else
-    maior = numero_secreto > chute.to_i
-    if maior
-        puts "Errou, o número secreto é maior!"
-    else
-        puts "Errou, o número secreto é menor!"
-    end
-end
-puts "Tentativa 3"
-puts "Entre com o numero"
-chute = gets
-puts "Será que acertou? Você chutou " + chute
-puts "\n\n\n\n"
-acertou = numero_secreto  == chute.to_i
-if acertou
-    puts "Acertou!"
-else
-    maior = numero_secreto > chute.to_i
-    if maior
-        puts "Errou, o número secreto é maior!"
-    else
-        puts "Errou, o número secreto é menor!"
+        end
     end
 end
