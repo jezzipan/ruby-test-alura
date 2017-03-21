f7c7e3d6e2450b975a6ef48993597cf5d551eb37
Your Name <you@example.com>
2017-03-21 11:32:29 -0300

    Extracao de codigo -da_boas_vindas-

--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -1,8 +1,14 @@
-puts "Bem-vindo ao jogo da adivinhação"
-puts "Qual é o seu nome?"
-nome = gets
-puts "\n\n\n\n\n\n"
-puts "Começaremos o jogo para você, " + nome
+def da_boas_vindas
+    puts "Bem-vindo ao jogo da adivinhação"
+    puts "Qual é o seu nome?"
+    nome = gets
+    puts "\n\n\n\n\n\n"
+    puts "Começaremos o jogo para você, " + nome
+end
+
+
+da_boas_vindas
+
 puts "Escolhendo um número secreto entre 0 e 200..."
 numero_secreto = 175
 puts "Escolhido... que tal adivinhar hoje nosso número secreto?"
