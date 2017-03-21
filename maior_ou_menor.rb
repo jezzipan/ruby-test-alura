d14255ffc9e3d27db49b17f3f4b097ea2c929da9
Your Name <you@example.com>
2017-03-21 00:55:34 -0300

    Criado um repositório para o jogo em ruby

---
+++ maior_ou_menor.rb
@@ -0,0 +1,16 @@
+puts "Bem-vindo ao jogo da adivinhação"
+puts "Qual é o seu nome?"
+nome = gets
+
+
+puts "Começaremos o jogo para você, " + nome
+puts "Escolhendo um número secreto entre 0 e 200..."
+numero_secreto = 175
+puts "Escolhido... que tal adivinhar hoje nosso número secreto?"
+puts
+puts
+puts
+puts "Tentativa 1"
+puts "Entre com o numero"
+chute = gets
+puts "Será que acertou? Você chutou " + chute
