15cefc47a3b20b657d7f3e7e2a105ba58af20301
Your Name <you@example.com>
2017-03-22 17:21:45 -0300

    Criando e manipulando arrays

--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -11,32 +11,42 @@ def sorteia_numero_secreto
     puts "Escolhido... que tal adivinhar hoje nosso número secreto?"
     return numero_secreto
 end
+def pede_um_numero(chutes, tentativa, limite_de_tentativas)
+    puts "\n\n\n\n"
+    puts "Tentativa " + tentativa.to_s + " de " + limite_de_tentativas.to_s
+    puts "Entre com o número"
+    puts "Chutes até agora: " + chutes.to_s
+    chute = gets
+    puts "Será que acertou? Você chutou " + chute
+    puts "Chutes até agora: " + chutes.to_s
+    chute.to_i
+end
+def verifica_se_acertou(numero_secreto, chute)
+    acertou = numero_secreto == chute
+    if acertou
+        puts "Acertou!"
+        return true
+    end
+        maior = numero_secreto > chute.to_i
+        if maior
+            puts "O número secreto é maior!"
+            else false
+            puts "O número secreto é menor!"
+            end
+            false
+end
 
 da_boas_vindas
-sorteia_numero_secreto
+numero_secreto = sorteia_numero_secreto
 limite_de_tentativas = 7
+chutes = []
+total_de_chutes = 0
 
-puts "Escolhendo um número secreto entre 0 e 200..."
-numero_secreto = 175
-puts "Escolhido... que tal adivinhar hoje nosso número secreto?"
-limite_de_tentativas = 7
 for tentativa in 1..limite_de_tentativas
-puts "\n\n\n\n"
-puts "Tentativas " + tentativa.to_s + "de 3"
-puts "Entre com o numero"
-chute = gets
-puts "Será que acertou? Você chutou " + chute
-puts "\n\n\n\n"
-acertou = numero_secreto  == chute.to_i
-if acertou
-    puts "Acertou!"
-    break
-else
-    maior = numero_secreto > chute.to_i
-    if maior
-        puts "Errou, o número secreto é maior!"
-    else
-        puts "Errou, o número secreto é menor!"
-        end
+    chute = pede_um_numero chutes, tentativa, limite_de_tentativas
+    chutes[total_de_chutes] = chute
+    total_de_chutes += 1
+    if verifica_se_acertou numero_secreto, chute
+        break
     end
 end
